d89f3f6da9c835ad89e2c1242126f99087ecb8bf
Philippe Daouadi <p.daouadi@free.fr>
2019-05-25 18:31:24 +0200

    TextTestFragment: change wrong answer feedback

--- app/src/main/java/org/kaqui/testactivities/TextTestFragment.kt
+++ app/src/main/java/org/kaqui/testactivities/TextTestFragment.kt
@@ -1,30 +1,40 @@
 package org.kaqui.testactivities
 
-import android.graphics.Rect
+import android.content.Context
 import android.os.Bundle
 import android.text.InputType
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.method.KeyListener
+import android.view.*
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
-import androidx.core.content.ContextCompat
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import org.jetbrains.anko.*
 import org.jetbrains.anko.support.v4.UI
 import org.kaqui.*
-import org.kaqui.model.*
+import org.kaqui.model.Certainty
+import org.kaqui.model.Kana
+import org.kaqui.model.getQuestionText
+import org.kaqui.model.text
+import android.view.inputmethod.InputMethodManager
 
 class TextTestFragment : Fragment(), TestFragment {
     companion object {
         private const val TAG = "TextTestFragment"
+
+        const val defaultInputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
 
         @JvmStatic
         fun newInstance() = TextTestFragment()
     }
 
     private lateinit var answerField: EditText
+    private var answerKeyListener: KeyListener? = null
+    private lateinit var correctAnswer: TextView
+    private var answer: String? = null
+    private lateinit var answerButtons: List<Button>
+    private lateinit var nextButton: Button
 
     private lateinit var testQuestionLayout: TestQuestionLayout
 
@@ -42,44 +52,61 @@ class TextTestFragment : Fragment(), TestFragment {
 
         val questionMinSize = 30
 
+        val answerButtons = mutableListOf<Button>()
+
         testQuestionLayout = TestQuestionLayout()
         val mainBlock = UI {
             testQuestionLayout.makeMainBlock(activity!!, this, questionMinSize, forceLandscape = true) {
                 wrapInScrollView(this) {
                     verticalLayout {
+                        correctAnswer = textView {
+                            textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+                            visibility = View.GONE
+                            textSize = 18f
+                            backgroundColor = context!!.getColorFromAttr(R.attr.correctAnswerBackground)
+                        }.lparams(width = matchParent, height = wrapContent)
+
                         answerField = editText {
                             gravity = Gravity.CENTER
-                            inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                            inputType = defaultInputType
                             setOnEditorActionListener { v, actionId, _ ->
                                 if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_NULL)
                                     this@TextTestFragment.onTextAnswerClicked(v, Certainty.SURE)
                                 else
                                     false
                             }
+                            answerKeyListener = keyListener
                         }.lparams(width = matchParent)
 
                         linearLayout {
-                            button(R.string.maybe) {
+                            val maybeButton = button(R.string.maybe) {
                                 setExtTint(R.color.answerMaybe)
                                 setOnClickListener {
                                     this@TextTestFragment.onTextAnswerClicked(this, Certainty.MAYBE)
                                 }
                             }.lparams(weight = 1f)
 
-                            button(R.string.sure) {
+                            val sureButton = button(R.string.sure) {
                                 setExtTint(R.color.answerSure)
                                 setOnClickListener {
                                     this@TextTestFragment.onTextAnswerClicked(this, Certainty.SURE)
                                 }
                             }.lparams(weight = 1f)
+                            answerButtons.add(maybeButton)
+                            answerButtons.add(sureButton)
                         }.lparams(width = matchParent, height = wrapContent)
 
                         linearLayout {
-                            button(R.string.dont_know) {
+                            val dontKnowButton = button(R.string.dont_know) {
                                 setExtTint(R.color.answerDontKnow)
                                 setOnClickListener { this@TextTestFragment.onTextAnswerClicked(this, Certainty.DONTKNOW) }
                             }.lparams(width = matchParent)
+                            answerButtons.add(dontKnowButton)
                         }.lparams(width = matchParent, height = wrapContent)
+
+                        nextButton = button(R.string.next) {
+                            setOnClickListener { this@TextTestFragment.onNextClicked() }
+                        }
                     }.lparams(width = matchParent, height = wrapContent)
                 }
             }
@@ -91,19 +118,67 @@ class TextTestFragment : Fragment(), TestFragment {
             true
         }
 
+        this.answerButtons = answerButtons
+
+        if (savedInstanceState != null) {
+            answer =
+                    if (savedInstanceState.containsKey("answer"))
+                        savedInstanceState.getString("answer")
+                    else
+                        null
+        }
+
         refreshQuestion()
 
         return mainBlock
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (answer != null)
+            outState.putString("answer", answer!!)
+    }
+
+    private fun refreshState() {
+        if (answer != null) {
+            answerField.setText(answer!!, TextView.BufferType.NORMAL)
+            answerField.backgroundColor = context!!.getColorFromAttr(R.attr.wrongAnswerBackground)
+            answerField.keyListener = null
+            correctAnswer.text = currentKana.romaji
+            correctAnswer.visibility = View.VISIBLE
+            for (button in answerButtons)
+                button.visibility = View.GONE
+            nextButton.visibility = View.VISIBLE
+        } else {
+            answerField.text.clear()
+            val attrs = context!!.obtainStyledAttributes(intArrayOf(android.R.attr.editTextBackground))
+            answerField.background = attrs.getDrawable(0)
+            attrs.recycle()
+            answerField.inputType = defaultInputType
+            correctAnswer.visibility = View.GONE
+            for (button in answerButtons)
+                button.visibility = View.VISIBLE
+            nextButton.visibility = View.GONE
+        }
+    }
+
     override fun refreshQuestion() {
         testQuestionLayout.questionText.text = testEngine.currentQuestion.getQuestionText(testType)
 
-        answerField.setText("")
+        refreshState()
+
         answerField.requestFocus()
+        val imm = context!!.getSystemService(Context.INPUT_METHOD_SERVICE)!! as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
     }
 
     private fun onTextAnswerClicked(view: View, certainty: Certainty): Boolean {
+        if (answer != null)
+            onNextClicked()
+
+        if (certainty == Certainty.DONTKNOW)
+            answerField.text.clear()
+
         val result =
                 if (certainty == Certainty.DONTKNOW) {
                     Certainty.DONTKNOW
@@ -121,8 +196,19 @@ class TextTestFragment : Fragment(), TestFragment {
                 }
 
         testFragmentHolder.onAnswer(view, result, null)
-        testFragmentHolder.nextQuestion()
+
+        if (result != Certainty.DONTKNOW)
+            testFragmentHolder.nextQuestion()
+        else {
+            answer = answerField.text.toString()
+            refreshState()
+        }
 
         return true
+    }
+
+    private fun onNextClicked() {
+        answer = null
+        testFragmentHolder.nextQuestion()
     }
 }
