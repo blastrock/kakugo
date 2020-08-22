699fa8ae9f9d069ad6be573fe21235c7ba05b70e
Philippe Daouadi <p.daouadi@free.fr>
2020-08-22 21:43:15 +0200

    Certainty: add converters to/from int

--- app/src/main/java/org/kaqui/model/Certainty.kt
+++ app/src/main/java/org/kaqui/model/Certainty.kt
@@ -1,7 +1,12 @@
 package org.kaqui.model
 
-enum class Certainty {
-    SURE,
-    MAYBE,
-    DONTKNOW,
+enum class Certainty(val value: Int) {
+    DONTKNOW(0),
+    MAYBE(1),
+    SURE(2);
+
+    companion object {
+        private val map = values().associateBy(Certainty::value)
+        fun fromInt(type: Int) = map.getValue(type)
+    }
 }
