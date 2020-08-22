b406fe743d1b60afa9b4dcf7cd5194d433f2eb34
Philippe Daouadi <p.daouadi@free.fr>
2020-08-22 21:43:05 +0200

    TestType: add converters to/from int

--- app/src/main/java/org/kaqui/model/TestType.kt
+++ app/src/main/java/org/kaqui/model/TestType.kt
@@ -1,29 +1,34 @@
 package org.kaqui.model
 
-enum class TestType {
-    HIRAGANA_TO_ROMAJI,
-    HIRAGANA_TO_ROMAJI_TEXT,
-    ROMAJI_TO_HIRAGANA,
+enum class TestType(val value: Int) {
+    HIRAGANA_TO_ROMAJI(1),
+    HIRAGANA_TO_ROMAJI_TEXT(2),
+    ROMAJI_TO_HIRAGANA(3),
 
-    HIRAGANA_DRAWING,
+    HIRAGANA_DRAWING(4),
 
-    KATAKANA_TO_ROMAJI,
-    KATAKANA_TO_ROMAJI_TEXT,
-    ROMAJI_TO_KATAKANA,
+    KATAKANA_TO_ROMAJI(5),
+    KATAKANA_TO_ROMAJI_TEXT(6),
+    ROMAJI_TO_KATAKANA(7),
 
-    KATAKANA_DRAWING,
+    KATAKANA_DRAWING(8),
 
-    KANJI_TO_READING,
-    READING_TO_KANJI,
-    KANJI_TO_MEANING,
-    MEANING_TO_KANJI,
+    KANJI_TO_READING(9),
+    READING_TO_KANJI(10),
+    KANJI_TO_MEANING(11),
+    MEANING_TO_KANJI(12),
 
-    WORD_TO_READING,
-    READING_TO_WORD,
-    WORD_TO_MEANING,
-    MEANING_TO_WORD,
+    WORD_TO_READING(13),
+    READING_TO_WORD(14),
+    WORD_TO_MEANING(15),
+    MEANING_TO_WORD(16),
 
-    KANJI_DRAWING,
+    KANJI_DRAWING(17),
 
-    KANJI_COMPOSITION,
+    KANJI_COMPOSITION(18);
+
+    companion object {
+        private val map = values().associateBy(TestType::value)
+        fun fromInt(type: Int) = map.getValue(type)
+    }
 }
